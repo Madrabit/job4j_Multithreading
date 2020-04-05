4cad867677b7203fb7fd47ed895872ebd1fa0465
madrabit <madrabot@gmail.com>
2020-04-05 15:32:28 +0300

    3. Прерывание нити[#266638]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,44 @@
+package ru.job4j.concurrent;
+
+/**
+ * @author madrabit
+ * Console Preloader.
+ * Showing Thread interrupt()
+ */
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Thread.sleep(500);
+                System.out.print("\r load: " + "-");
+                Thread.sleep(500);
+                System.out.print("\r load: " + "\\");
+                Thread.sleep(500);
+                System.out.print("\r load: " + "|");
+                Thread.sleep(500);
+                System.out.print("\r load: " + "/");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Thread progress = new Thread(
+                new ConsoleProgress()
+        );
+        progress.start();
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            progress.interrupt();
+        }
+        progress.interrupt();
+
+    }
+
+}
