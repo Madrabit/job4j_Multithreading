dbdb60ded230361c9c19b207ca55b9091ca376cd
madrabit <madrabot@gmail.com>
2020-04-05 14:25:32 +0300

    1.2. Режим ожидания.[#266636]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,27 @@
+package ru.job4j.concurrent;
+
+/**
+ * @author madrabit
+ * Simulation of load process. Using Thread sleep().
+ */
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        System.out.println("Start loading ... ");
+                        Thread.sleep(1000);
+                        for (int i = 0; i <= 100; i++) {
+                            Thread.sleep(500);
+                            System.out.print("\rLoading : " + i + "%");
+                        }
+                        System.out.println(System.lineSeparator() + "Loaded.");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+        System.out.println("Main");
+    }
+}
