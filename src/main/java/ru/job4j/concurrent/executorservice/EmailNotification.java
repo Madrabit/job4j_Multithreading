37aebea047a358fb365bd6b34869c4c3517278e0
madrabit <madrabot@gmail.com>
2020-04-16 12:58:27 +0300

    2. ExecutorService рассылка почты.[#266625]

---
+++ src/main/java/ru/job4j/concurrent/executorservice/EmailNotification.java
@@ -0,0 +1,58 @@
+package ru.job4j.concurrent.executorservice;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author madrabit
+ * Email sender with Thread Pool.
+ */
+public class EmailNotification {
+
+    /**
+     * Pool of Threads.
+     */
+    private final ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+    /**
+     * Convert Notification string and Send.
+     *
+     * @param user Target user.
+     */
+    public void emailTo(User user) {
+        pool.submit(() -> {
+            String subject = String.format("Notification %s to email %s.", user.getUsername(), user.getEmail());
+            String body = String.format("Add a new event to %s", user.getUsername());
+            send(subject, body, user.getEmail());
+        });
+
+    }
+
+    /**
+     * Close pool.
+     */
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Send prepared email.
+     *
+     * @param subject Email subject.
+     * @param body    Email text body.
+     * @param email   @mail.
+     */
+    public void send(String subject, String body, String email) {
+
+    }
+
+}
