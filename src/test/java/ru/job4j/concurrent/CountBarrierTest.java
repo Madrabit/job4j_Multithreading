1adac2cdd6ed3ed2014f6880bad51776238ecfab
madrabit <madrabot@gmail.com>
2020-04-21 20:01:28 +0300

    CountBarrier - сделал тест проверяет, что второй поток работает.

--- src/test/java/ru/job4j/concurrent/CountBarrierTest.java
+++ src/test/java/ru/job4j/concurrent/CountBarrierTest.java
@@ -3,15 +3,18 @@ package ru.job4j.concurrent;
 import org.junit.jupiter.api.Test;
 import ru.job4j.concurrent.barrier.CountBarrier;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author madrabit
  */
 public class CountBarrierTest {
+    private Thread.State slaveState;
+
     @Test
     public void test() throws InterruptedException {
-        boolean result = false;
         CountBarrier barrier = new CountBarrier(100);
         Thread master = new Thread(
                 () -> {
@@ -23,15 +26,14 @@ public class CountBarrierTest {
         Thread slave = new Thread(
                 () -> {
                     barrier.await();
+                    slaveState = Thread.currentThread().getState();
                     System.out.println(Thread.currentThread().getName() + " started");
+                    assertEquals(Thread.State.RUNNABLE, Thread.currentThread().getState());
                 },
                 "Slave"
         );
         master.start();
         slave.start();
         master.join();
-        
-        assertEquals(Thread.State.WAITING, slave.getState());
-
     }
 }
